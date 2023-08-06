3b24cbaca0103cd9efc67f21a1bb50a5e55378d7
Sam Newitt <samuel.newitt@gmail.com>
2023-08-05 20:43:13 -0500

    Finished period on panel

--- web/.resources/fbPanelScripts.js
+++ web/.resources/fbPanelScripts.js
@@ -1,4 +1,10 @@
-window.onbeforeunload=function(){}
+// window.onbeforeunload = function(event) {
+//     event.returnValue = "Whoa there! Leaving so soon??? O:";
+//   };
+
+function e(param){
+    return document.getElementById(param);
+}
 
 var socket;
 
@@ -10,14 +16,14 @@ function serverConnect(){
             
 socket.addEventListener("open", function (event) {
 console.log("Websocket Connected!"); 
-document.getElementById("wait-cover").style.display="none";
+e("wait-cover").style.display="none";
 
 });
 
 
 socket.addEventListener("close", function (event) {
 console.log("Websocket Disconnected :(");
-document.getElementById("wait-cover").style.display="none";
+e("wait-cover").style.display="none";
 setTimeout(function() {
     serverConnect();
   }, 1000);
@@ -27,14 +33,18 @@ setTimeout(function() {
 
 serverConnect();
 
+function send(param){
+    socket.send(param);
+}
+
 // ------------------------ TIMER CODE ----------------------------------------------
 
 var clockRemain=720, clock="12:00", input;
 
 function clockStart(){
-    socket.send("clock-start");
-    document.getElementById("clock-start").classList.add("active");
-    document.getElementById("clock-stop").classList.remove("active");
+    send("clock-start");
+    e("clock-start").classList.add("active");
+    e("clock-stop").classList.remove("active");
     clockInterval = setInterval(function(){
         clockRemain--;
         if(clockRemain%60<10){
@@ -43,7 +53,7 @@ function clockStart(){
         else{
         clock=Math.trunc(clockRemain/60)+":"+clockRemain%60;
         }
-document.getElementById("clock").innerHTML=clock;
+e("clock").innerHTML=clock;
 if(clockRemain==0)
 {
     clockStop();
@@ -54,10 +64,10 @@ if(clockRemain==0)
 
 function clockStop(){
     clearInterval(clockInterval);
-    socket.send("clock-stop");
-    socket.send("clockVal="+clockRemain);
-    document.getElementById("clock-start").classList.remove("active");
-    document.getElementById("clock-stop").classList.add("active");
+    send("clock-stop");
+    send("clockVal="+clockRemain);
+    e("clock-start").classList.remove("active");
+    e("clock-stop").classList.add("active");
 }
 
 function clockSet(param){
@@ -68,18 +78,18 @@ if(clockRemain%60<10){
 else{
 clock=Math.trunc(clockRemain/60)+":"+clockRemain%60;
 }
-document.getElementById("clock").innerHTML=clock;
+e("clock").innerHTML=clock;
 }
 
 function clockUpdate(){
-input = document.getElementById("clock-input").value;
+input = e("clock-input").value;
 input=input.replace(";",":");
 if(input.includes(":")){
 input = input.split(":");
 clockSet(parseInt(input[0])*60+parseInt(input[1]));
 }
 else{
-document.getElementById("clock-input").value="Error";
+e("clock-input").value="Error";
 }
 }
 
@@ -89,15 +99,87 @@ function clockReset(){
 
 //----------------------------------------------------QUARTER CODE------------------------------------
 
-var period=1, finalizeds;
+var period=1, finalized;
 
+function updatePeriod(){
+send("period="+period);
+switch (period){
+    case 0:
+        break;
+    case 1:
+        e("period").innerHTML="1st";
+    break;
+    case 1.5:
+        e("period").innerHTML="End 1st";
+    break;
+
+    case 2:
+        e("period").innerHTML="2nd";
+    break;
+    case 2.5:
+        e("period").innerHTML="Half";
+    break;
+
+    case 3:
+        e("period").innerHTML="3rd";
+    break;
+    case 3.5:
+        e("period").innerHTML="End 3rd";
+    break;
+
+    case 4:
+        e("period").innerHTML="4th";
+    break;
+    case 4.5:
+        e("period").innerHTML="End Reg";
+    break;
+
+    case 5:
+        e("period").innerHTML="OT";
+    break;
+    case 5.5:
+        e("period").innerHTML="End 1OT";
+    break;
+
+    default:
+    if(period%1==0){
+        e("period").innerHTML=period-4+"OT";
+    }
+    else{
+        e("period").innerHTML="End "+(Math.trunc(period)-4)+"OT";
+        
+    }
+    break;
+    
+}
+}
 
 function nextPeriod(){
-period+=0.5;
+if(!finalized){
+    period+=0.5;
 updatePeriod();
+}
 }
 
 function prevPeriod(){
+   if(finalized)
+   {
+    finalized=false;
+    updatePeriod();
+    e("final").classList.remove("active");
+   }
+    else if(period!=1){
     period-=0.5;
     updatePeriod();
     }
+    }
+
+function final(){
+    if (!finalized){
+        e("period").innerHTML="Final";
+        send ("final");
+        e("final").classList.add("active");
+        finalized=true;
+    }
+    
+}
