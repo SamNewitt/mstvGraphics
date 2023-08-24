4d1eaabc82b93cfc8789dd4e5d4c7f9576ae93a8
Sam Newitt <samuel.newitt@gmail.com>
2023-08-23 19:50:27 -0500

    Updated custom transition

--- web/.resources/fbDisplay/custom.js
+++ web/.resources/fbDisplay/custom.js
@@ -2,13 +2,13 @@ function customIn(param){
     graphicIsLive=true;
     graphicLive="custom";
     e("custom").setAttribute("src","customImages/"+param+".png");
-    e("custom").style.animation="customIn 0.75s ease 0s";
+    e("custom-container").style.animation="customIn 0.75s ease 0s";
     // e("custom").style.animation="opacityIn 5s linear 0s";
 }
 
 function customOut(){
     // e("custom").style.animation="opacityOut 5s linear 0s";
-    e("custom").style.animation="customOut 0.75s ease 0s";
+    e("custom-container").style.animation="customOut 0.75s ease 0s";
 
 
 }
