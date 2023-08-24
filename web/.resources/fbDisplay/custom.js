9e4382457595998282fd583c4b2f1b50b18054ea
Sam Newitt <samuel.newitt@gmail.com>
2023-08-23 23:18:28 -0500

    More updates from full test

--- web/.resources/fbDisplay/custom.js
+++ web/.resources/fbDisplay/custom.js
@@ -2,13 +2,13 @@ function customIn(param){
     graphicIsLive=true;
     graphicLive="custom";
     e("custom").setAttribute("src","customImages/"+param+".png");
-    e("custom-container").style.animation="customIn 0.75s ease 0s";
+    e("custom-container").style.animation="customIn 1.5s ease 0s";
     // e("custom").style.animation="opacityIn 5s linear 0s";
 }
 
 function customOut(){
     // e("custom").style.animation="opacityOut 5s linear 0s";
-    e("custom-container").style.animation="customOut 0.75s ease 0s";
+    e("custom-container").style.animation="customOut 1.5s ease 0s";
 
 
 }
