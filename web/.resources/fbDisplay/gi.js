176ad325a9176e3c9d696875c07f4303ab24b230
Sam Newitt <samuel.newitt@gmail.com>
2023-09-14 23:41:41 -0500

    Fixed bug with text not resizing on game intro

--- web/.resources/fbDisplay/gi.js
+++ web/.resources/fbDisplay/gi.js
@@ -41,5 +41,8 @@ function giIn() {
     else{
         e("home-desc").innerHTML="(#"+jsonData.home.seed+", "+jsonData.home.record+")";
     }
+    adjustText(e("gi-away-name"),e("gi-away-name-container"),45,35);
+    adjustText(e("gi-home-name"),e("gi-home-name-container"),45,35);
+
     giOut();
   }
