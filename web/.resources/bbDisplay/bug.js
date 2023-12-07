7865c1e39342f6d9aa21664589107e53f84b62fa
Sam Newitt <samuel.newitt@gmail.com>
2023-12-07 16:36:08 -0600

    Added bug js for score

---
+++ web/.resources/bbDisplay/bug.js
@@ -0,0 +1,104 @@
+window.onload = bugOut();
+
+
+
+function bugAnimate() {
+    graphicIsLive=true;
+    graphicLive="bug";
+    document.getElementById("bug-animate-cover").style.maxWidth = "0px";
+    document.getElementById("bug-container").style.maxWidth = "0px";
+    document.getElementById("bug-animate-cover").style.animation =
+      "bugWipe 0.5s ease-out 0s";
+  
+    document.getElementById("bug-animate-text").style.animation =
+      "bugAnimateText 1.75s linear 0s";
+  
+    document.getElementById("bug-container").style.animation =
+      "bugWipe 0.5s ease-out 1.25s";
+    setTimeout(bugAnimateSet, 2000);
+  }
+  
+  function bugAnimateSet() {
+    document.getElementById("bug-animate-cover").style.maxWidth = "1280px";
+    document.getElementById("bug-container").style.maxWidth = "1280px";
+    document.getElementById("bug-animate-text").style.animation = "";
+    bugSponsors();
+  }
+  
+  function bugOut() {
+    clearInterval( sponsorsInterval);
+    graphicIsLive=false;
+    awayPopupOut();
+    homePopupOut();
+    document.getElementById("bug-animate-cover").style.animation =
+      "bugOut 0.5s ease-in 0s";
+    document.getElementById("bug-container").style.animation =
+      "bugOut 0.5s ease-in 0s";
+  
+    document.getElementById("away-popup").style.animation =
+      "bugOut 0.5s ease-in 0s";
+    document.getElementById("home-popup").style.animation =
+      "bugOut 0.5s ease-in 0s";
+    document.getElementById("away-popup-container").style.animation =
+      "bugOut 0.5s ease-in 0s";
+    document.getElementById("home-popup-container").style.animation =
+      "bugOut 0.5s ease-in 0s";
+  }
+  
+  function bugIn() {
+    graphicIsLive=true;
+    graphicLive="bug";
+    document.getElementById("bug-container").style.animation =
+      "bugIn 0.5s ease-out 0s";
+      bugSponsors();
+  }
+  
+  function homePopupIn(param) {
+    clearTimeout(homePopupTimeout);
+    e("home-popup-text").innerHTML=param;
+    adjustText(e("home-popup-text"),e("home-popup-container"),32,20);
+    document.getElementById("home-popup").style.animation =
+      "popupIn 0.2s linear 0s";
+    document.getElementById("home-popup-container").style.animation =
+      "popupIn 0.2s linear 0s";
+  }
+  
+  function homePopupOut() {
+    document.getElementById("home-popup").style.animation =
+      "popupOut 0.2s linear 0s";
+    document.getElementById("home-popup-container").style.animation =
+      "popupOut 0.2s linear 0s";
+  }
+  
+  function awayPopupIn(param) {
+    clearTimeout(awayPopupTimeout);
+    adjustText(e("away-popup-text"),e("away-popup-container"),32,20);
+    document.getElementById("away-popup").style.animation =
+      "popupIn 0.2s linear 0s";
+    document.getElementById("away-popup-container").style.animation =
+      "popupIn 0.2s linear 0s";
+  }
+  
+  function awayPopupOut() {
+    document.getElementById("away-popup").style.animation =
+      "popupOut 0.2s linear 0s";
+    document.getElementById("away-popup-container").style.animation =
+      "popupOut 0.2s linear 0s";
+  }
+  
+  var sponsorsInterval, currSponsor=0;
+
+  var sponsors=["avera", "dnn", "fox", "gs", "amFam","cb", "ilcc","jtw","ls", "avera", "dnn", "fox", "gs", "mstvWide","sds","shenanigans","touchmark","vern"];
+  e("sponsor-rotator").setAttribute("src","sponsors/"+sponsors[0]+".png")
+
+
+  function bugSponsors(){
+    sponsorsInterval = setInterval(function(){
+      currSponsor++;
+      if(currSponsor==sponsors.length){
+        currSponsor=0;
+      }
+      e("sponsor-rotator").setAttribute("src","sponsors/"+sponsors[currSponsor]+".png")
+    },15000)
+  }
+ 
---
+++ web/.resources/bbDisplay/gameData.js
@@ -0,0 +1,179 @@
+var opacityOut="opacityOut 0.2s linear 0s", opacityIn="opacityIn 0.2s linear 0s";
+//----------------------------------------TIMER-----------------------------------------------------
+
+var clockRemain=480, clock="8:00";
+
+function clockStart(){
+    clockInterval = setInterval(function(){
+        clockRemain--;
+        if(clockRemain%60<10){
+            clock=Math.trunc(clockRemain/60)+":0"+clockRemain%60;
+        }
+        else{
+        clock=Math.trunc(clockRemain/60)+":"+clockRemain%60;
+        }
+setHTML("clock",clock);
+if(clockRemain==0)
+{
+    clockStop();
+}
+},1000);
+}
+
+function clockStop(){
+    clearInterval(clockInterval);
+}
+
+function clockSet(param){
+clockRemain=param;
+if(clockRemain%60<10){
+    clock=Math.trunc(clockRemain/60)+":0"+clockRemain%60;
+}
+else{
+clock=Math.trunc(clockRemain/60)+":"+clockRemain%60;
+}
+setHTML("clock",clock);
+}
+
+//----------------------------------------------------------QUARTER-------------------------------------------------
+
+var clockObjects=c("clock");
+
+function period(param){
+if(param.includes("End")||param == "Halftime" || param=="Final" || param.includes("OT")){
+    for(var i=0; i<clockObjects.length; i++){
+        clockObjects[i].style.display="none";
+    }
+}
+else{
+    for(var i=0; i<clockObjects.length; i++){
+        clockObjects[i].style.display="block";
+    }
+}
+setHTML("period",param);
+}
+
+
+//------------------------------------------------------------SCORE-----------------------------------------
+var prevAwayScore=0, prevHomeScore=0;
+
+function awayScore(param){
+    if(param>prevAwayScore){
+    e("bug-away-score").style.animation=opacityOut;
+    setTimeout(function(){
+        setHTML("away-score",param);
+    e("bug-away-score").style.animation=opacityIn;
+    },500);
+}
+else{
+    setHTML("away-score",param);
+}
+prevAwayScore=param;
+}
+
+function homeScore(param){
+    if(param>prevHomeScore){
+    e("bug-home-score").style.animation=opacityOut;
+    setTimeout(function(){
+        setHTML("home-score",param);
+    e("bug-home-score").style.animation=opacityIn;
+    },500);
+}
+else{
+    setHTML("home-score",param);
+}
+prevHomeScore=param;
+}
+
+//-----------------------------------------------------TIMEOUT CODE---------------------------------------------
+
+var awayPopupTimeout, homePopupTimeout, awayTORemaining=5, homeTORemaining=5, awayTOBubbles=c("away-to"), homeTOBubbles=c("home-to");
+
+function awayTakeTO(){
+    if(graphicIsLive && graphicLive=="bug"){
+  awayPopupIn("TIMEOUT", false);
+  awayTOBubbles[awayTORemaining-1].style.animation="awayFlashTO 0.75s linear 0s 6";
+
+  awayPopupTimeout= setTimeout(function(){
+    awayPopupOut();
+  },4000)
+  setTimeout(function(){
+    awayTOBubbles[awayTORemaining-1].classList.remove("remain");
+    awayTOBubbles[awayTORemaining-1].style.animation="";
+    awayTORemaining--;
+  },4125)
+}
+else{
+    awayTO(awayTORemaining-1);
+}
+ 
+}
+
+function awayTO(param){
+    awayTOBubbles[0].classList.remove("remain");
+    awayTOBubbles[1].classList.remove("remain");
+    awayTOBubbles[2].classList.remove("remain");
+    awayTOBubbles[3].classList.remove("remain");
+    awayTOBubbles[4].classList.remove("remain");
+
+    awayTORemaining=param;
+    if(param>0){
+    awayTOBubbles[0].classList.add("remain");
+    }
+    if(param>1){
+        awayTOBubbles[1].classList.add("remain");
+        }
+    if(param>2){
+    awayTOBubbles[2].classList.add("remain");
+    }
+    if(param>3){
+        awayTOBubbles[3].classList.add("remain");
+        }
+    if(param>4){
+            awayTOBubbles[4].classList.add("remain");
+     }
+}
+
+function homeTakeTO(){
+    if(graphicIsLive && graphicLive=="bug"){
+        homePopupIn("TIMEOUT", false);
+  homeTOBubbles[homeTORemaining-1].style.animation="homeFlashTO 0.75s linear 0s 6";
+
+  homePopupTimeout= setTimeout(function(){
+    homePopupOut();
+  },4000)
+  setTimeout(function(){
+    homeTOBubbles[homeTORemaining-1].classList.remove("remain");
+    homeTOBubbles[homeTORemaining-1].style.animation="";
+    homeTORemaining--;
+  },4125)
+}
+else{
+    homeTO(homeTORemaining-1);
+}
+ 
+}
+
+function homeTO(param){
+    homeTOBubbles[0].classList.remove("remain");
+    homeTOBubbles[1].classList.remove("remain");
+    homeTOBubbles[2].classList.remove("remain");
+    homeTOBubbles[3].classList.remove("remain");
+    homeTOBubbles[4].classList.remove("remain");
+    homeTORemaining=param;
+    if(param>0){
+    homeTOBubbles[0].classList.add("remain");
+    }
+    if(param>1){
+        homeTOBubbles[1].classList.add("remain");
+        }
+    if(param>2){
+        homeTOBubbles[2].classList.add("remain");
+    }
+    if(param>3){
+        homeTOBubbles[3].classList.add("remain");
+    }
+    if(param>4){
+        homeTOBubbles[4].classList.add("remain");
+    }
+}
